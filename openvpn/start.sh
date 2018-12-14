c5a3e47d5ec0c8a2b3bb6a375953fca4b6a962fb
MarkusMcNugen <marknewton5@gmail.com>
2018-12-14 00:16:04 -0500

    Add 5 second sleep to allow openvpn to connect first

--- openvpn/start.sh
+++ openvpn/start.sh
@@ -163,6 +163,8 @@ if [[ $VPN_ENABLED == "yes" ]]; then
 	echo "[info] Starting OpenVPN..." | ts '%Y-%m-%d %H:%M:%.S'
 	cd /config/openvpn
 	exec openvpn --config ${VPN_CONFIG} &
+	# give openvpn some time to connect
+	sleep 5
 	#exec /bin/bash /etc/openvpn/openvpn.init start &
 	exec /bin/bash /etc/qbittorrent/iptables.sh
 else
